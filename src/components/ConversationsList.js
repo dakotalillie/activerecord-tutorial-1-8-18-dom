1879699c2d64990ded469019d26cf7a099b464b1
Dakota Lillie <dakota.lillie@icloud.com>
2018-01-07 12:55:16 -0500

    init

---
+++ src/components/ConversationsList.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import { ActionCable } from 'react-actioncable-provider';
+import { API_ROOT } from '../constants';
+import NewConversationForm from './NewConversationForm';
+import MessagesArea from './MessagesArea';
+import Cables from './Cables';
+
+class ConversationsList extends React.Component {
+  state = {
+    conversations: [],
+    activeConversation: null
+  };
+
+  componentDidMount = () => {
+    fetch(`${API_ROOT}/conversations`)
+      .then(res => res.json())
+      .then(conversations => this.setState({ conversations }));
+  };
+
+  handleClick = id => {
+    this.setState({ activeConversation: id });
+  };
+
+  handleReceivedConversation = response => {
+    const { conversation } = response;
+    this.setState({
+      conversations: [...this.state.conversations, conversation]
+    });
+  };
+
+  handleReceivedMessage = response => {
+    const { message } = response;
+    const conversations = [...this.state.conversations];
+    const conversation = conversations.find(
+      conversation => conversation.id === message.conversation_id
+    );
+    conversation.messages = [...conversation.messages, message];
+    this.setState({ conversations });
+  };
+
+  render = () => {
+    const { conversations, activeConversation } = this.state;
+    return (
+      <div className="conversationsList">
+        <ActionCable
+          channel={{ channel: 'ConversationsChannel' }}
+          onReceived={this.handleReceivedConversation}
+        />
+        {this.state.conversations.length ? (
+          <Cables
+            conversations={conversations}
+            handleReceivedMessage={this.handleReceivedMessage}
+          />
+        ) : null}
+        <h2>Conversations</h2>
+        <ul>{mapConversations(conversations, this.handleClick)}</ul>
+        <NewConversationForm />
+        {activeConversation ? (
+          <MessagesArea
+            conversation={findActiveConversation(
+              conversations,
+              activeConversation
+            )}
+          />
+        ) : null}
+      </div>
+    );
+  };
+}
+
+export default ConversationsList;
+
+// helpers
+
+const findActiveConversation = (conversations, activeConversation) => {
+  return conversations.find(
+    conversation => conversation.id === activeConversation
+  );
+};
+
+const mapConversations = (conversations, handleClick) => {
+  return conversations.map(conversation => {
+    return (
+      <li key={conversation.id} onClick={() => handleClick(conversation.id)}>
+        {conversation.title}
+      </li>
+    );
+  });
+};
---
+++ src/components/MessagesArea.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import NewMessageForm from './NewMessageForm';
+
+const MessagesArea = ({
+  conversation: { id, title, messages },
+}) => {
+  return (
+    <div className="messagesArea">
+      <h2>{title}</h2>
+      <ul>{orderedMessages(messages)}</ul>
+      <NewMessageForm conversation_id={id} />
+    </div>
+  );
+};
+
+export default MessagesArea;
+
+// helpers
+
+const orderedMessages = messages => {
+  const sortedMessages = messages.sort(
+    (a, b) => new Date(a.created_at) - new Date(b.created_at)
+  );
+  return sortedMessages.map(message => {
+    return <li key={message.id}>{message.text}</li>;
+  });
+};
