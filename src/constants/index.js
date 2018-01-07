3b9d8330d6a12860c69c4258ddd4d0046f8a3576
Dakota Lillie <dakota.lillie@icloud.com>
2018-01-07 13:30:58 -0500

    heroku

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
-export const API_ROOT = 'http://localhost:3000';
-export const API_WS_ROOT = 'ws://localhost:3000/cable';
+export const API_ROOT = 'https://actioncable-tutorial-api.herokuapp.com';
+export const API_WS_ROOT = 'wss://actioncable-tutorial-api.herokuapp.com/cable';
 export const HEADERS = {
   'Content-Type': 'application/json',
   Accept: 'application/json',
